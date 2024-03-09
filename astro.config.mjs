fda2c0c402fd6d3e0a69d34370e38cf665066201
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-09 08:00:49 -0800

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightGhostCMS from '@matthiesenxyz/starlight-ghostcms';
+import houston from './src/styles/houston.json';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://matthiesen.xyz',
+	integrations: [
+		starlight({
+			title: 'MatthiesenXYZ',
+			description: 'MatthiesenXYZ - Open Source Software Development & Support',
+			tagline: 'Open Source Software Development & Support',
+			favicon: 'mxyz-logo.png',
+			components: {
+				SiteTitle: './src/starlight/SiteTitle.astro',
+			},
+			expressiveCode: { themes: [ houston ] },
+			logo: {
+				src: './src/assets/mxyz-logo.png',
+				alt: 'MatthiesenXYZ',
+				replacesTitle: true,
+			},
+			social: {
+				github: 'https://github.com/matthiesenxyz',
+				gitlab: 'https://gitlab.com/matthiesenxyz',
+			},
+			customCss: ['./src/styles/starlight.css'],
+			plugins: [ 
+				starlightGhostCMS({
+					ghostURL: "https://ghost.matthiesen.xyz",
+					title: "MatthiesenXYZ",
+					rssDescription: "MatthiesenXYZ - Open Source Software Development & Support",
+				}),
+			],
+			sidebar: [
+				{
+					label: 'Astro Integrations',
+					autogenerate: { directory: 'astro-integrations' },
+				},
+				{
+					label: 'GitHub Workflow Management',
+					autogenerate: { directory: 'git-workflows' },
+				}
+			],
+		}),
+	],
+});
---
+++ src/content/docs/astro-integrations/index.md
@@ -0,0 +1,13 @@
+---
+title: About our Integrations
+sidebar:
+  order: 1
+---
+
+The integrations in this list come from two Github sources,
+- [`MatthiesenXYZ`](https://github.com/matthiesenxyz) - The Main Github Organization
+- [`AdamMatthiesen`](https://github.com/adammatthiesen) - Adam's Github Profile
+
+Check the sidebar or use the arrow buttons below to continue.
+
+Note: Some of our projects are also mirrored to [`GitLab`](https://gitlab.com/matthiesenxyz)
